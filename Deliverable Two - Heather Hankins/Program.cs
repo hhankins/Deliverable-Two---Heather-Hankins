88a065f0b2b5bee3d08fd6c1a61f87cec8b512b8
Heather <hmhankins03@gmail.com>
2021-10-27 22:28:23 -0400

    Update

--- Deliverable Two - Heather Hankins/Program.cs
+++ Deliverable Two - Heather Hankins/Program.cs
@@ -6,13 +6,16 @@ namespace Deliverable_Two___Heather_Hankins
     {
         static void Main(string[] args)
         {
+            bool somethingElse = true; 
+            
             Console.WriteLine("Hello! My name is ChatBot. I hope you are doing well today.");
             Console.WriteLine();
 
-            while ()
+            do
+            {
                 Console.WriteLine("Is there something you would like to say to me?");
-                Console.WriteLine();
-                string userInput1 = Console.ReadLine();
+                string userInput = Console.ReadLine();
+                string userInput1 = userInput.ToLower();
 
                 if (userInput1 == "bye")
                 {
@@ -24,6 +27,7 @@ namespace Deliverable_Two___Heather_Hankins
                     Console.WriteLine();
                     Console.WriteLine("Hi good to see you");
                     Console.WriteLine();
+                    Console.WriteLine();
                 }
 
                 else if (userInput1 == "sup")
@@ -31,21 +35,32 @@ namespace Deliverable_Two___Heather_Hankins
                     Console.WriteLine();
                     Console.WriteLine("I am good");
                     Console.WriteLine();
+                    Console.WriteLine();
                 }
 
-                else (userInput1 == "hello there")
+                else if (userInput1 == "hello there")
                 {
                     Console.WriteLine();
                     Console.WriteLine("General Kenobi");
                     Console.WriteLine();
+                    Console.WriteLine();
                 }
-        }
+
+            } while (somethingElse);
+
+            Console.WriteLine("Is there something you would like to say to me?");
+            string somethingElse1 = Console.ReadLine();
+
+            if (somethingElse1.ToLower() == "hello" || somethingElse1.ToLower() == "sup" || somethingElse1.ToLower() == "hello there")
+            {
+                somethingElse = true;
+            }
+            else
+            {
+                somethingElse = false;
+            }
+
+        } 
     }
 }
-
-
-
-
-        //Console.WriteLine("I'm sorry, but you have already said that. Is there something else you would like to say to me?");
-
-      
+        //Console.WriteLine("I'm sorry, but you have already said that."); 
